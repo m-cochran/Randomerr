41317269752f460607b1f15d7f7480082e3ebbc3
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-08-19 01:03:01 -0400

    Update custom_filters.rb

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -3,7 +3,6 @@
 module Jekyll
   module CustomFilters
     def extract_thumbnail(content)
-      # Use a regex to find the first image URL
       match = content.match(/<img[^>]+src="([^">]+)"/)
       match ? match[1] : nil
     end
