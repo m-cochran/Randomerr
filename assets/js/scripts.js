9bafd09339650d23c3e0ea53af57194b36e6fb12
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-08-19 00:27:07 -0400

    Create scripts.js

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// assets/js/scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+  const slider = document.querySelector('.slide-track');
+  
+  slider.addEventListener('mouseover', function () {
+    slider.style.animationPlayState = 'paused';
+  });
+  
+  slider.addEventListener('mouseout', function () {
+    slider.style.animationPlayState = 'running';
+  });
+});
