4bda6f3ae3a52f994661df98a311c74b25a93ece
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-08-22 23:04:06 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  // Toggle Navigation Menu
+  const navToggle = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('nav ul');
+
+  if (navToggle) {
+    navToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('nav-open');
+    });
+  }
