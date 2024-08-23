ab6b0a41dfcd1d9c61b6a6c049eb1eabf40ea9bd
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-08-23 09:44:52 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 // MAIN MENU
 const mainMenu = document.getElementById('mainMenu');
 const autoNav = document.getElementById('autoNav');
