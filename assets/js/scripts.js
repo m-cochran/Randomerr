20c64aad46d52a9859bfe5ce3bb977b439f5fdac
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-08-23 04:00:39 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,18 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Select the toggle button and the navigation menu
-  const navToggle = document.querySelector('.nav-toggle');
-  const navMenu = document.querySelector('nav ul');
+// MAIN MENU
+const $mainMenu = $("#mainMenu");
+const $autoNav = $("#autoNav");
+const $autoNavMore = $("#autoNavMore");
+const $autoNavMoreList = $("#autoNavMoreList");
+autoNavMore = () => {
+  let childNumber = 2;
 
-  // Define the toggleMenu function
-  function toggleMenu() {
-    navMenu.classList.toggle('nav-open');
+  if ($(window).width() >= 320) {
+    // GET MENU AND NAV WIDTH
+    const $menuWidth = $mainMenu.width();
+    const $autoNavWidth = $autoNav.width();
+    if ($autoNavWidth > $menuWidth) {
+      // CODE FIRES WHEN WINDOW SIZE GOES DOWN
+      $autoNav
+        .children(`li:nth-last-child(${childNumber})`)
+        .prependTo($autoNavMoreList);
+      autoNavMore();
+    } else {
+      // CODE FIRES WHEN WINDOW SIZE GOES UP
+      const $autoNavMoreFirst = $autoNavMoreList
+      .children("li:first-child")
+      .width();
+      // CHECK IF ITEM HAS ENOUGH SPACE TO PLACE IN MENU
+      if ($autoNavWidth + $autoNavMoreFirst < $menuWidth) {
+        $autoNavMoreList.children("li:first-child").insertBefore($autoNavMore);
+      }
+    }
+    if ($autoNavMoreList.children().length > 0) {
+      $autoNavMore.show();
+      childNumber = 2;
+    } else {
+      $autoNavMore.hide();
+      childNumber = 1;
+    }
   }
+};
+// INIT
+autoNavMore();
+$(window).resize(autoNavMore);
+// MAIN MENU END
 
-  // Add click event listener to the toggle button
-  if (navToggle) {
-    navToggle.addEventListener('click', toggleMenu);
-  }
-});
 
 
 
