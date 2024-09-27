6a010cbd02cc41b4799e3da8d663edd2dea9c1af
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-09-27 19:49:37 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,27 +37,6 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", autoNavMoreHandler);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".slide-track");
-
-  slider.addEventListener("mouseover", function () {
-    slider.style.animationPlayState = "paused";
-  });
-
-  slider.addEventListener("mouseout", function () {
-    slider.style.animationPlayState = "running";
-  });
-});
-
-  // Ensure anchor tags work properly
-  const anchors = slider.querySelectorAll("a");
-  anchors.forEach(anchor => {
-    anchor.addEventListener("click", function(event) {
-      // Ensure the default action is taken (i.e., opening the link)
-      event.stopPropagation(); // Stop event from bubbling up
-    });
-  });
-});
 
 
 
