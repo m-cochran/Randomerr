7ca9937314d673e18a1ad44a1589a968c9c3cec5
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-09-30 12:10:43 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -37,17 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", autoNavMoreHandler);
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const slider = document.querySelector(".slide-track");
 
-  slider.addEventListener("mouseover", function () {
-    slider.style.animationPlayState = "paused";
-  });
-
-  slider.addEventListener("mouseout", function () {
-    slider.style.animationPlayState = "running";
-  });
-});
 
 
 
