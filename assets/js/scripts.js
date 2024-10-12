86089a5cb623396003baea678a82132285be7a70
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-10-12 03:52:14 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -138,6 +138,16 @@ function initAutoNav() {
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const backgroundContainer = document.getElementById('backgroundContainer');
+    const shapeOverlay = document.getElementById('shapeOverlay');
+
+    if (!shapeOverlay) {
+        console.error("Shape overlay element not found!");
+        return;
+    }
+
+
 const animalImages = [
     'https://plus.unsplash.com/premium_photo-1675432656807-216d786dd468?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1495594059084-33752639b9c3?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
