e7a827ecfae25d70f19600b2234ddb20c83d84f2
m-cochran <106445623+m-cochran@users.noreply.github.com>
2024-10-14 10:08:46 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -200,7 +200,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // October
     "37week.img",
-    "<img src="{{ site.baseurl }}/assets/images/header-backgrounds/halloween-week38.webp" alt="{{ site.title }} halloween-week38">",
+    "https://m-cochran.github.io/Randomerr/assets/images/header-backgrounds/halloween-week38.webp" alt="{{ site.title }} halloween-week38",
     "https://images.pexels.com/photos/3095465/pexels-photo-3095465.png",
     "40week.img",
 
