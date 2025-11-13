1ba2312644377fd8b1e7a8fcf83b24b477928507
m-cochran <106445623+m-cochran@users.noreply.github.com>
2025-11-13 18:03:17 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -460,7 +460,6 @@ function changeImage(thumb) {
     player.loadVideoById(video.videoId);
     videoTitle.innerHTML = `<a href="https://www.youtube.com/watch?v=${video.videoId}" target="_blank">🎥 ${video.title}</a>`;
     thumbContainer.querySelectorAll("img").forEach((t, i) => t.classList.toggle("active", i === currentIndex));
-    thumbContainer.querySelectorAll("img")[currentIndex].scrollIntoView({ behavior: "smooth", inline: "center" });
   }
 
   function onPlayerStateChange(event) {
@@ -471,3 +470,4 @@ function changeImage(thumb) {
   }
 
 })(); // <-- closes the IIFE
+
